80537f270276d81e927dc5a10feec9d6b2a70315
Taylor Savage <tjsavage@stanford.edu>
2012-03-08 19:50:13 -0800

    Initial Commit

---
+++ create.sql
@@ -0,0 +1,74 @@
+USE c_cs108_tjsavage;
+
+CREATE TABLE IF NOT EXISTS users(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(id),
+	username VARCHAR(40) NOT NULL,
+	password VARCHAR(65) NOT NULL,
+	isAdmin INT
+);
+
+CREATE TABLE IF NOT EXISTS friends_join(
+	friend1ID  INT UNSIGNED NOT NULL REFERENCES users(id),
+	friend2ID INT UNSIGNED NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS quizzes(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(id),
+	name VARCHAR(40) NOT NULL,
+	description TEXT,
+	creator INT UNSIGNED NOT NULL REFERENCES users(id),
+	ordered BOOLEAN,
+	multi_page BOOLEAN,
+	date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS questions(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(id),
+	quizID INT UNSIGNED NOT NULL REFERENCES quizzes(id),
+	question_type INT NOT NULL,
+	specific_questionID INT UNSIGNED NOT NULL,
+	order_index INT
+);
+
+CREATE TABLE IF NOT EXISTS question_response_questions(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(id),
+	questionID INT UNSIGNED NOT NULL REFERENCES questions(id),
+	question_text TEXT
+);
+
+CREATE TABLE IF NOT EXISTS fill_in_questions(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(id),
+	questionID INT UNSIGNED NOT NULL REFERENCES questions(id),
+	question_text TEXT
+);
+
+CREATE TABLE IF NOT EXISTS multiple_choice_questions(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(id),
+	questionID INT UNSIGNED NOT NULL REFERENCES questions(id),
+	question_text TEXT
+);
+
+CREATE TABLE IF NOT EXISTS multiple_choice_choices(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(id),
+	questionID INT UNSIGNED NOT NULL REFERENCES questions(id),
+	choice VARCHAR(60)
+);
+
+CREATE TABLE IF NOT EXISTS picture_response_questions(
+	
+);
+
+CREATE TABLE IF NOT EXISTS answers(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(id),
+	questionID INT UNSIGNED NOT NULL REFERENCES questions(id),
+	answer VARCHAR(60)
+);
+
---
+++ src/models/Quiz.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Quiz {
+	public enum QUIZ_TYPE {
+		QUESTION_RESPONSE,
+		
+	}
+	private String name;
+	private String description;
+	private int creatorID;
+}
