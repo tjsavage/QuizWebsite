231e6fbdec3cb70ef2391a1036d41228f74b5e81
Taylor Savage <tjsavage@stanford.edu>
2012-03-09 03:02:58 -0800

    Added drop.sql

---
+++ drop.sql
@@ -0,0 +1,24 @@
+USE c_cs108_tjsavage;
+
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS friends_join;
+
+DROP TABLE IF EXISTS friend_requests;
+
+DROP TABLE IF EXISTS quizzes;
+
+DROP TABLE IF EXISTS questions;
+
+DROP TABLE IF EXISTS question_response_questions;
+
+DROP TABLE IF EXISTS fill_in_questions;
+
+DROP TABLE IF EXISTS multiple_choice_questions;
+
+DROP TABLE IF EXISTS multiple_choice_choices;
+
+DROP TABLE IF EXISTS picture_response_questions;
+
+DROP TABLE IF EXISTS answers;
+
