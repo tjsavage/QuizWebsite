8f5fa24566df155fa112698ca1b158d4a1e32b83
Taylor Savage <tjsavage@stanford.edu>
2012-03-09 03:02:58 -0800

    Added drop.sql

---
+++ drop.sql
@@ -0,0 +1,24 @@
+USE c_cs108_tjsavage;
+
+DROP TABLE users;
+
+DROP TABLE friends_join;
+
+DROP TABLE friend_requests;
+
+DROP TABLE quizzes;
+
+DROP TABLE questions;
+
+DROP TABLE question_response_questions;
+
+DROP TABLE fill_in_questions;
+
+DROP TABLE multiple_choice_questions;
+
+DROP TABLE multiple_choice_choices;
+
+DROP TABLE picture_response_questions;
+
+DROP TABLE answers;
+
